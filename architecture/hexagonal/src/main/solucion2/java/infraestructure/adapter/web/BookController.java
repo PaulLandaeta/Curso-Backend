cb77e95d6f3adb70a4d59fa2e3fb08ddb93ef1f2
AnderMichael <andercaymi@gmail.com>
2023-11-08 13:19:28 -0400

    Tarea en clases! Tienda de Libros Apaza - Cayllan

---
+++ architecture/hexagonal/src/main/solucion2/java/infraestructure/adapter/web/BookController.java
@@ -0,0 +1,7 @@
+package main.solucion2.java.infraestructure.adapter.web;
+
+//@RestController
+//@RequestMapping("/books")
+public class BookController {
+    // TODO: Aqui va el controlador del CRUD y la venta
+}
