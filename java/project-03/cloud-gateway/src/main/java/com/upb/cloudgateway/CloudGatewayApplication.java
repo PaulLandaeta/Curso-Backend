3486a237e1b16c8b590987a492a565b1f695b213
Paul Landaeta <paulwilkerlf@gmail.com>
2023-12-08 12:49:21 -0400

    add redis and okta security

--- java/project-03/cloud-gateway/src/main/java/com/upb/cloudgateway/CloudGatewayApplication.java
+++ java/project-03/cloud-gateway/src/main/java/com/upb/cloudgateway/CloudGatewayApplication.java
@@ -2,6 +2,10 @@ package com.upb.cloudgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class CloudGatewayApplication {
@@ -9,5 +13,15 @@ public class CloudGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CloudGatewayApplication.class, args);
 	}
+	@Bean
+	KeyResolver userKeySolver(){
+		// return exchange -> Mono.just("userKey");
+		return exchange -> Mono.just(exchange.getRequest().getRemoteAddress().getHostName());
+	}
 
+	@Bean
+	public RedisRateLimiter rateLimiter() {
+		return new RedisRateLimiter(3, 3);
+		// replenishRate, burstCapacity, intervalo en segundos
+	}
 }
