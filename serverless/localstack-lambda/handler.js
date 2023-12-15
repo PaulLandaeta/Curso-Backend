0d7684f253947043292399b24a6a1f1b19e10cb8
Paul Landaeta <paulwilkerlf@gmail.com>
2023-12-15 14:07:36 -0400

    serverless

---
+++ serverless/localstack-lambda/handler.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports.hello = async (event) => {
+  // Logica de Negocio 
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
